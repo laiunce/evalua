e8d2fda36044bc6602ffae9a9be542fcfeec1dd6
cristian laiun <laiun.ce@gmail.com>
2018-03-24 12:55:15 -0300

    initial commit

---
+++ solucion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 12:08:00 2018
+
+@author: laiunce
+"""
+
+# ---------- > WORKINGDIRECTORY
+import os
+working_directory = '/Users/laiunce/Documents/evalua'
+os.chdir(working_directory)
+# < ---------- WORKINGDIRECTORY
