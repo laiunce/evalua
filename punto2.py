b36949fb90fe67d06aa3b2199d41ab4009f6f3d2
cristian laiun <laiun.ce@gmail.com>
2018-03-24 15:17:22 -0300

    punto 3

--- punto2.py
+++ punto2.py
@@ -20,6 +20,45 @@ import os
 from datetime import datetime, timedelta
 
 
+#descompone la serie para investigar ruido
+from random import randrange
+from pandas import Series
+from matplotlib import pyplot
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+def analizar_serie_outlier(analiza_serie,porcentaje_cota):
+    series = [analiza_serie[i] for i in range(1,len(analiza_serie))]
+    # graficamos la serie observando tomando de 3 dias cuales son los componentes estacionales, de tendencia y de ruuido
+    result = seasonal_decompose(series, model='additive', freq=3)
+    result.plot()
+    pyplot.show()
+    
+    #si el nivel de ruido super porcentaje respecto al valor obsoluto del maximo pico entonces es anomalo
+    
+    
+    residuos = result.resid
+    
+    
+    list_sinnan=[]
+    for i in residuos[1:len(residuos)-1]:
+        list_sinnan.append(i)
+    
+    
+    #obtiene el maximo valor absoluto
+    cota  = max(abs(max(list_sinnan)),abs(min(list_sinnan)))
+
+    
+    lista_indices = []
+    for i in range(0,len(residuos)):
+        if abs(residuos[i]) > cota*porcentaje_cota:
+            lista_indices.append(i)
+            
+    for i in  lista_indices:
+        print('pos:'+str(i)+', valor: '+str(analiza_serie[i]))
+
+    
+    
 diasdelay= 30
 
 
@@ -46,7 +85,7 @@ v_avg = df[['1b. open (USD)', '4b. close (USD)']].mean(axis=1)
 import matplotlib.pyplot as plt
 
 
-
+#plotea
 
 plt.plot(v_open, color='g')
 plt.plot(v_close, color='b')
@@ -57,5 +96,30 @@ plt.title('cotizaciones y promedio')
 plt.show()
 
 
+# PUNTO 3
 
+#LE INGRESA LA COTA para hacer tecnica por descomposicion si el residuo supera un 50% del maximo desvio lo tilda como outlier de la siere
+
+analizar_serie_outlier(v_open,0.5)
+
+analizar_serie_outlier(v_close,0.5)
+
+analizar_serie_outlier(v_open,0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
