42e41c616458eb027bea81398d3b13372ddb92e3
cristian laiun <laiun.ce@gmail.com>
2018-03-24 13:43:25 -0300

    bajada

--- solucion.py
+++ solucion.py
@@ -11,3 +11,43 @@ import os
 working_directory = '/Users/laiunce/Documents/evalua'
 os.chdir(working_directory)
 # < ---------- WORKINGDIRECTORY
+
+
+from pandas.io.json import json_normalize
+import requests
+import pandas as pd
+import numpy as np
+import os
+
+
+
+
+
+# la api no tiene filtro de fechas por lo que el filtrado se realiza una vez descargada la informacion
+
+
+
+moneda='BTC'
+mercado = 'CNY'
+
+path = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={}&market={}&apikey=demo#jso".format(moneda,mercado)
+r = requests.get(path) 
+json = r.json()
+
+
+data_por_tiempo = json['Time Series (Digital Currency Daily)']
+
+
+dataset = pd.DataFrame() 
+for i in data_por_tiempo:
+    try:
+        #formato año mes dia se eliminan guiones para tener un incremental independiente de la fecha
+        norm=json_normalize(data_por_tiempo[i])
+        norm['fecha'] = i.replace('-','')
+        dataset = dataset.append(norm)
+    except:
+        pass
+    
+    
+
+
